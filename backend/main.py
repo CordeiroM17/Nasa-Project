0a663c1f419b313931642a02bf837081dcae60a0
FacuSB <blancofacundo777@gmail.com>
2025-10-03 20:32:43 -0300

    Refactor Vercel configuration and update app to run in production mode

--- backend/main.py
+++ backend/main.py
@@ -13,4 +13,4 @@ api.add_resource(MeteorsResource, '/api/meteors')
 api.add_resource(Simulation, '/api/simulate-impact')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
