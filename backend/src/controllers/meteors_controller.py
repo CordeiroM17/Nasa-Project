11336b5c2cbbb159c94b1e0a263269feb3195307
artorias <jerojournade1@gmail.com>
2025-10-04 19:10:59 -0300

    switch pagination to scroll in asteroid list

--- backend/src/controllers/meteors_controller.py
+++ backend/src/controllers/meteors_controller.py
@@ -11,14 +11,19 @@ meteors_bp = Blueprint('meteors', __name__, url_prefix='/api')
 def ok():
     return("ok")
 
+
 @meteors_bp.route('/meteors', methods=['GET'])
 def meteors():
     # Leer query params
     page = int(request.args.get("page", 1))       # si no pasa page → 1
-    per_page = int(request.args.get("per_page", 5))  # si no pasa per_page → 5
+    per_page = int(request.args.get("per_page", 20)) 
 
     df, status_code = get_earth_meteors()
+    print(f"DEBUG: DataFrame size before formatting: {len(df)}") 
+    
     result = format_response(df, status_code, page=page, per_page=per_page)
+    print(f"DEBUG: Data size being sent in JSON: {len(result.get('data', []))}") 
+    
     return jsonify(result), status_code
 
 @meteors_bp.route('/simulate-impact', methods=['POST', 'OPTIONS'])
